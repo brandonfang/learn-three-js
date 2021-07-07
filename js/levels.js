fe2f6e88e0e837da9f55f344610bfcb33e156afa
brandonfang <bdmfang@gmail.com>
2021-07-07 12:00:00 -0700

    Add fonts

--- js/levels.js
+++ js/levels.js
@@ -3,7 +3,7 @@ let levels = [
     number: 1,
     name: 'level 1',
     instructions: '<h2>Welcome! This is a game to help you learn about <a href="https://threejs.org/" target="_blank">three.js</a> and 3D graphics.</h2><p>Instructions content for level 1. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla urna nisl, rhoncus eget bibendum at, euismod eget nibh. In elit arcu, malesuada a diam vel, imperdiet gravida mi. Vivamus ante neque, blandit ut efficitur sed, fermentum quis tellus. Nulla est elit, semper in finibus at, finibus in sapien. Nulla bibendum leo ac orci varius, eget fermentum nulla efficitur.</p><p>In tempus, lacus quis malesuada consectetur adipiscing elit. Nulla urna nisl, rhoncus eget bibendum at, euismod eget nibh. In elit arcu, malesuada a diam vel, imperdiet gravida mi. Vivamus ante neque, blandit feugiat, orci justo vehicula sapien, in blandit orci massa eu nunc. Duis sit amet elit diam. Morbi tincidunt orci et mi fermentum, eget lacinia odio cursus. Pellentesque at quam facilisis, euismod purus sed, efficitur quam.</p>',
-    reference: '<ul><li><a href="" target="_blank">Reference item one</a></li><li><a href="" targt="_blank">Reference item two</a></li></ul>',
+    reference: '<ol><li><a href="" target="_blank">Reference item one</a></li><li><a href="" targt="_blank">Reference item two</a></li></ol>',
     board: '',
     selector: '',
     style: {},
