51dada6a6ddb406ee3343312e6cebec503eb03da
brandonfang <bdmfang@gmail.com>
2021-07-27 12:00:00 -0700

    Update levels and use minified three.js

--- js/levels.js
+++ js/levels.js
@@ -1,46 +1,51 @@
 let levels = [
-  { 
+  {
     number: 1,
     name: 'level 1',
-    instructions: '<h2>Welcome! This is a game to help you learn about <a href="https://threejs.org/" target="_blank">three.js</a> and 3D graphics.</h2><p>Instructions content for level 1. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla urna nisl, rhoncus eget bibendum at, euismod eget nibh. In elit arcu, malesuada a diam vel, imperdiet gravida mi. Vivamus ante neque, blandit ut efficitur sed, fermentum quis tellus. Nulla est elit, semper in finibus at, finibus in sapien. Nulla bibendum leo ac orci varius, eget fermentum nulla efficitur.</p><p>In tempus, lacus quis malesuada consectetur adipiscing elit. Nulla urna nisl, rhoncus eget bibendum at, euismod eget nibh. In elit arcu, malesuada a diam vel, imperdiet gravida mi. Vivamus ante neque, blandit feugiat, orci justo vehicula sapien, in blandit orci massa eu nunc. Duis sit amet elit diam. Morbi tincidunt orci et mi fermentum, eget lacinia odio cursus. Pellentesque at quam facilisis, euismod purus sed, efficitur quam.</p>',
-    reference: '<ol><li><a href="" target="_blank">Reference item one</a></li><li><a href="" targt="_blank">Reference item two</a></li></ol>',
+    instructions:
+      '<h2>Welcome! This is a game to help you learn about three.js.</h2><p>WebGL is a JavaScript API for rendering interactive 2D and 3D graphics without the use of plug-ins. WebGL is very fast, allowing GPU-accelerated usage of physics and image processing and effects as part of the web page canvas. It makes </p><p>Three.js is an easy to use, lightweight, cross-browser, general purpose 3D library built on top of WebGL. It does most of the heavy lifting (shaders and matrices) for us and allows us to write more human-friendly code.</p>',
+    reference:
+      '<ol><li><a href="https://github.com/mrdoob/three.js/" target="_blank">Three.js GitHub Source Code</a></li><li><a href="https://threejs.org" target="_blank">Three.js official documentation</a></li><li><a href="https://threejsfundamentals.org/" target="_blank">Three.js Fundamentals</a></li></ol>',
     board: '',
     selector: '',
     style: {},
     before: '',
-    after: ''
+    after: '',
   },
   {
     number: 2,
     name: 'level 2',
-    instructions: '<h2>Instructions title for level 2.</h2><p>Instructions for level 2. Lorem ipsum dolor sit ametcu, malesuada a diam vel, imperdiet gravida mi. Vivamus ante neque, blandit ut efficitur sed, fermentum quis tellus. Nulla est elit, semper in finibus at, finibus in sapien. Nulla bibendum leo ac orci varius, eget fermentum nulla efficitur. In tempus, lacus quis malesuada feugiat, ismod purus sed, efficitur quam.</p>',
+    instructions:
+      '<h2>Instructions title for level 2.</h2><p>Instructions for level 2. Lorem ipsum dolor sit ametcu, malesuada a diam vel, imperdiet gravida mi. Vivamus ante neque, blandit ut efficitur sed, fermentum quis tellus. Nulla est elit, semper in finibus at, finibus in sapien. Nulla bibendum leo ac orci varius, eget fermentum nulla efficitur. In tempus, lacus quis malesuada feugiat, ismod purus sed, efficitur quam.</p>',
     reference: '<p>This is the reference material for level 2.</p>',
     board: '',
     selector: '',
     style: {},
     before: '',
-    after: ''
+    after: '',
   },
   {
     number: 3,
     name: 'level 3',
-    instructions: '<h2>Instructions title for level 3.</h2><p>Instructions for level 3. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla urna nisl, rhoncus eget bibendum at, euismod eget nibh. In elit arcu, malesuada a diam vel, imperdiet gravida mi. Vivamus ante neque, blandit ut efficitur sed, fermentum quiius, eget fermentum nulla efficitur. In tempus, lacus quis malesuada feugiat, orci justo vehicula sapien, in blandit orci massa eu nunc. Duis sit amet elit diam. Morbi tincidunt orci et mi fermentum, eget lacinia odio cursus. Pellentesque at quam facilisis, euismod purus sed, efficitur quam.</p>',
+    instructions:
+      '<h2>Instructions title for level 3.</h2><p>Instructions for level 3. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla urna nisl, rhoncus eget bibendum at, euismod eget nibh. In elit arcu, malesuada a diam vel, imperdiet gravida mi. Vivamus ante neque, blandit ut efficitur sed, fermentum quiius, eget fermentum nulla efficitur. In tempus, lacus quis malesuada feugiat, orci justo vehicula sapien, in blandit orci massa eu nunc. Duis sit amet elit diam. Morbi tincidunt orci et mi fermentum, eget lacinia odio cursus. Pellentesque at quam facilisis, euismod purus sed, efficitur quam.</p>',
     reference: '<p>This is the reference material for level 3.</p>',
     board: '',
     selector: '',
     style: {},
     before: '',
-    after: ''
+    after: '',
   },
   {
     number: 4,
     name: 'level 4',
-    instructions: '<h2>Instructions title for level 4.</h2><p>Instructions for level 4. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla urna nisl, rhoncus eget bibendum at, euismod eget fermentum quis tellus. Nulla est elit, semper in finibus at, finibus in sapien. Nulla bibendum leo ac orci varius nibh. In elit arcu, malesuada a diam vel, imperdiet gravida mi. Vivamus ante neque, blandit ut efficitur sed, fermentum quis tellus. Nulla est elit, semper in finibus at, finibus in sapien. Nulla bibendum leo ac orci varius, eget fermentum nulla efficitur. In tempus, lacus quis malesuada feugiat, orci justo vehicula sapien, in blandit orci massa eu nunc. Duis sit amet elit diam. Morbi tincidunt orci et mi fermentum, eget lacinia odio cursus. Pellentesque at quam facilisis, euismod purus sed, efficitur quam.</p>',
+    instructions:
+      '<h2>Instructions title for level 4.</h2><p>Instructions for level 4. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla urna nisl, rhoncus eget bibendum at, euismod eget fermentum quis tellus. Nulla est elit, semper in finibus at, finibus in sapien. Nulla bibendum leo ac orci varius nibh. In elit arcu, malesuada a diam vel, imperdiet gravida mi. Vivamus ante neque, blandit ut efficitur sed, fermentum quis tellus. Nulla est elit, semper in finibus at, finibus in sapien. Nulla bibendum leo ac orci varius, eget fermentum nulla efficitur. In tempus, lacus quis malesuada feugiat, orci justo vehicula sapien, in blandit orci massa eu nunc. Duis sit amet elit diam. Morbi tincidunt orci et mi fermentum, eget lacinia odio cursus. Pellentesque at quam facilisis, euismod purus sed, efficitur quam.</p>',
     reference: '<p>This is the reference material for level 4.</p>',
     board: '',
     selector: '',
     style: {},
     before: '',
-    after: ''
+    after: '',
   },
 ];
