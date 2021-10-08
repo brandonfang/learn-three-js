711ba26291ed8a706716ddf390d8993b7517817e
brandonfang <bdmfang@gmail.com>
2021-10-08 11:16:27 -0700

    Add rough canvas

---
+++ js/canvas.js
@@ -0,0 +1,34 @@
+const container = document.getElementById('canvas');
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  container.clientWidth / container.clientHeight,
+  0.1,
+  1000
+);
+
+const renderer = new THREE.WebGLRenderer({ alpha: true });
+renderer.setSize(container.clientWidth, container.clientHeight);
+renderer.setClearColor(0xfdc48b, 1);
+container.appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({ color: 0x34d399 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+const animate = function () {
+  requestAnimationFrame(animate);
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+};
+
+animate();
+
+console.log('teststst')
